b94578c61d26862cb484449b09767b2fc1640fe0
2ing <2ingsowtf@gmail.com>
2018-04-05 15:34:23 +0800

    答辩之前最后一次修改

--- notebook/src/test/java/com/notebook/config/mail/EmailTest.java
+++ notebook/src/test/java/com/notebook/config/mail/EmailTest.java
@@ -20,7 +20,6 @@ public class EmailTest {
 	@Test
 	public void sendMail(){
 		try {
-			//EmailUtil.emailUtilSet("smtp.exmail.qq.com", "2ing@2inger.top", "Qq7788990");
 			
 			EmailUtil.sentEmail("252631706@qq.com", "关于2ing对于我司产品的投诉", "投诉结果：无效，详情请见，www.xxx.com/wtf");
 		} catch (GeneralSecurityException e) {
@@ -35,19 +34,5 @@ public class EmailTest {
 		
 	}
 	
-//	@Test
-//	public void connectMail(){
-//		try {
-////			System.out.println(EmailConfig.getHost());
-////			System.out.println(EmailConfig.getUsername());
-////			System.out.println(EmailConfig.getPassword());
-//			EmailUtil.emailUtilSet(EmailConfig.getHost(), EmailConfig.getUsername(), EmailConfig.getPassword());
-//			//EmailUtil.emailUtilSet("smtp.exmail.qq.com", "2ing@2inger.top", "Qq7788990");
-//			EmailUtil.connectTest();
-//			System.out.println("连接成功");
-//		}catch (Exception e) {
-//			e.printStackTrace();
-//			System.out.println("连接失败");
-//		}
-//	}
+
 }
